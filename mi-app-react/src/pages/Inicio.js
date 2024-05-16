5d2749c9bb8547dc0bcc82eb57f415f68f70cd7c
unknown <camendez632@ucatolica.edu.co>
2024-05-15 19:19:19 -0500

    pagina web arriba

---
+++ mi-app-react/src/pages/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../components/Main';  // Asegúrate de ajustar la ruta si tu estructura es diferente
+
+function Inicio() {
+    return (
+        <div className="inicio">
+            <Main />
+        </div>
+    );
+}
+
+export default Inicio;
